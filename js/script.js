36b5f558010e4e9b299562c6d52875512c4be516
Kenny-Le-281 <99355017+Kenny-Le-281@users.noreply.github.com>
2022-03-31 17:22:09 +0000

    changes to script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@
  * Created on: Mar 2022
  * This file contains the JS functions for index.html 
 */
+
+'use strict'
+
+/**
+ * This function calculates the volume of the pyramid.
+ */
+function calculate () {
+  // input
+  const length = parseFloat(document.getElementById('pyramid-length').value)
+  const width = parseFloat(document.getElementById('pyramid-width').value)
+  const height = parseFloat(document.getElementById('pyramid-height').value)
+
+  // process
+  const volume = (length * width * height) / 3
+
+  // output
+  document.getElementById('volume').innerHTML = 'The volume of the pyramid is: ' + volume.toFixed(2) + ' cm²'
+}
